9e73797225a8546c75d672083ace5fc8a99f8440
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-09 14:26:28 +0100

    fix inordered bug to print the characters in same order as the endpoint

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,27 +4,27 @@ const request = require('request');
 const arg = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${arg}`;
 
-function getRequest(url) {
-    return new Promise((resolve, reject) => {
-      request(url, (error, response, body) => {
-        if (error) reject(error)
+function getRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) reject(error);
 
-        resolve(JSON.parse(body))
-      })
-    })
+      resolve(JSON.parse(body));
+    });
+  });
 }
 
-async function getCharacters() {
+async function getCharacters () {
   try {
-    const body = await getRequest(url)
-    const characters = body.characters
+    const body = await getRequest(url);
+    const characters = body.characters;
     for (const characterUrl of characters) {
-      character = await getRequest(characterUrl)
-      console.log(character.name)
+      const character = await getRequest(characterUrl);
+      console.log(character.name);
     }
   } catch (error) {
-      console.error(error)
+    console.error(error);
   }
 }
 
-getCharacters()
+getCharacters();
