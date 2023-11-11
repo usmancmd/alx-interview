0ccd9eaa3984747d6c0e2c47034af4397295d973
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-11 17:16:37 +0100

    Revert "fix inordered bug to print the characters in same order as the endpoint"
    This reverts commit 9e73797225a8546c75d672083ace5fc8a99f8440.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,43 +4,27 @@ const request = require('request');
 const movie = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movie}`;
 
-const makeRequest = (characterUrl) => {
+function getRequest (url) {
   return new Promise((resolve, reject) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        const characterObj = JSON.parse(body);
-        resolve(characterObj.name);
-      }
+    request(url, (error, response, body) => {
+      if (error) reject(error);
+
+      resolve(JSON.parse(body));
     });
   });
-};
+}
 
-request(url, (error, response, body) => {
-  if (error) {
+async function getCharacters () {
+  try {
+    const body = await getRequest(url);
+    const characters = body.characters;
+    for (const characterUrl of characters) {
+      const character = await getRequest(characterUrl);
+      console.log(character.name);
+    }
+  } catch (error) {
     console.error(error);
-  } else {
-    const movieObj = JSON.parse(body);
-    const characterUrls = movieObj.characters;
-
-    // Recursive function to process characterUrls one by one
-    const processCharacter = (index) => {
-      if (index >= characterUrls.length) {
-        return; // Base case: all characters processed
-      }
-
-      makeRequest(characterUrls[index])
-        .then((characterName) => {
-          console.log(characterName);
-          processCharacter(index + 1); // Process next character
-        })
-        .catch((error) => {
-          console.error(error);
-          processCharacter(index + 1); // Process next character
-        });
-    };
-
-    processCharacter(0); // Start processing from the first character
   }
-});
+}
+
+getCharacters();
