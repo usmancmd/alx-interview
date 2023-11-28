0ae5029f30ffe49ca4f130784f6cb42d8dec32d8
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-28 09:09:24 +0100

    Add edge cases

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,6 +13,14 @@ def island_perimeter(grid):
             count += 1
         elif sum(v) == 1:
             count += 2
+        elif sum(v) == 2:
+            count += 4
         elif sum(v) == 3:
             count += 6
+        elif sum(v) == 4:
+            count += 8
+        elif sum(v) == 5:
+            count += 10
+        elif sum(v) == 6:
+            count += 12
     return count
