f6bbb9305233ccc7806a0a40e18df59771764be8
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-28 11:52:32 +0100

    fix edge cases

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,20 +7,34 @@ Calculate the perimeter of a island in grid
 def island_perimeter(grid):
     """Returns island perimeter"""
     perimeter = 0
-    count = 0
     for i, v in enumerate(grid):
-        if sum(v) == 0:
-            count += 1
-        elif sum(v) == 1:
-            count += 2
-        elif sum(v) == 2:
-            count += 4
-        elif sum(v) == 3:
-            count += 6
-        elif sum(v) == 4:
-            count += 8
-        elif sum(v) == 5:
-            count += 10
-        elif sum(v) == 6:
-            count += 12
-    return count
+        for j, k in enumerate(v):
+            if k == 1:
+                if j == 0:
+                    perimeter += 1
+                    if v[j + 1] == 0:
+                        perimeter += 1
+                elif j == len(grid[0]):
+                    if v[j - 1] == 0:
+                        perimeter += 1
+                    perimeter += 1
+                else:
+                    if v[j - 1] == 0:
+                        perimeter += 1
+                    if v[j + 1] == 0:
+                        perimeter += 1
+
+                if i == 0:
+                    perimeter += 1
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
+                elif i == len(grid):
+                    if grid[i - 1][j] == 0:
+                        perimeter += 1
+                    perimeter += 1
+                else:
+                    if grid[i - 1][j] == 0:
+                        perimeter += 1
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
+    return perimeter
